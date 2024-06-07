5a5e070a17d20e44acc675b919fcf5879f9cb909
Jhonatan Felix <jhonatanvfelix@gmail.com>
2024-06-07 16:14:00 -0300

    Implementado o tratamento de exceptions

--- src/main/java/com/compass/backend/services/UserService.java
+++ src/main/java/com/compass/backend/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.compass.backend.entities.User;
 import com.compass.backend.repositories.UserRepository;
+import com.compass.backend.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -22,7 +23,7 @@ public class UserService {
 	
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
---
+++ src/main/java/com/compass/backend/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,12 @@
+package com.compass.backend.services.exceptions;
+
+public class ResourceNotFoundException  extends RuntimeException{
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundException(Object id) {
+		super("Recurso não encontrado. Id " + id);
+	}
+
+}
